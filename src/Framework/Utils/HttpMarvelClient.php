c875b164c0fa145353ba121cdce5f0403c64d809
emmanuelc <camuschinox@gmail.com>
2024-01-02 21:48:03 -0300

    removed credentials

--- src/Framework/Utils/HttpMarvelClient.php
+++ src/Framework/Utils/HttpMarvelClient.php
@@ -9,15 +9,16 @@ use GuzzleHttp\Client as MarvelClient;
 class HttpMarvelClient
 {
 
-    private const MARVEL_API_BASE = "https://gateway.marvel.com/v1/public/";
-
-    private const MARVEL_PUBLIC_KEY = "ec22c0fe40cb820063cbc0d47b09b2f9";
-
-    private const MARVEL_PRIVATE_KEY = "4c97ac9eba1aa23bd3bcd870067a393e37ca1f82";
+    private $baseApiPath;
+    private $publicApiKey;
+    private $privateApiKey;
 
     public function __construct(
         private MarvelClient $client = new MarvelClient()
     ) {
+        $this->baseApiPath = getenv("BASE_API_PATH");
+        $this->publicApiKey = getenv("PUBLIC_API_KEY");
+        $this->privateApiKey = getenv("PRIVATE_API_KEY");
     }
 
     public function getAllHeroes(): array
@@ -34,11 +35,11 @@ class HttpMarvelClient
     {
         $ts = strval(time());
         $hash = $this->getHashCode($ts);
-        $publicApiKey = self::MARVEL_PUBLIC_KEY;
+        $publicApiKey = $this->publicApiKey;
 
         return sprintf(
             "%s%s?ts=%s&apikey=%s&hash=%s",
-            self::MARVEL_API_BASE,
+            $this->baseApiPath,
             $topic,
             $ts,
             $publicApiKey,
@@ -48,6 +49,6 @@ class HttpMarvelClient
 
     private function getHashCode(string $timeStamp): string
     {
-        return md5($timeStamp . self::MARVEL_PRIVATE_KEY . self::MARVEL_PUBLIC_KEY);
+        return md5($timeStamp . $this->privateApiKey . $this->publicApiKey);
     }
 }
